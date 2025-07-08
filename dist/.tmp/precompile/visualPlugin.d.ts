edb2273ea444bc574f887b5b49978b6531ea78bb
lproko <127095474+lproko@users.noreply.github.com>
2025-07-08 15:20:04 +0300

    Delete dist directory

--- dist/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var leafletMapVisualD689974AB3D948E7B4B17ECA36CEEE89_DEBUG: IVisualPlugin;
-export default leafletMapVisualD689974AB3D948E7B4B17ECA36CEEE89_DEBUG;
